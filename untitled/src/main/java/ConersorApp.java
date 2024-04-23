45a2a311979c6bf9c20e9383eaba9f280fdaa40d
suhellynaraujo <github.interface@gmail.com>
2024-04-23 20:51:10 -0300

    criando classe app

---
+++ untitled/src/main/java/ConersorApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConersorApp {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("***********************************************************");
+
+        System.out.println("Bem vindo(a) ao conversor de moedas!");
+
+        System.out.println("***********************************************************");
+
+    }
+}
