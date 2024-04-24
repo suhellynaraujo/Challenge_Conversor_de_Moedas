79a5af8a64765e501aa60c135d4698376ac37447
suhellynaraujo <github.interface@gmail.com>
2024-04-23 21:35:02 -0300

    model

--- untitled/src/main/java/model/ConversorModel.java
+++ untitled/src/main/java/model/ConversorModel.java
@@ -1,3 +1,36 @@
 package model;
 public class ConversorModel {
+    String nomeMoeda;
+    String tipoMoeda;
+    String localMoeda;
+
+    public ConversorModel(String nomeMoeda, String tipoMoeda, String localMoeda) {
+        this.nomeMoeda = nomeMoeda;
+        this.tipoMoeda = tipoMoeda;
+        this.localMoeda = localMoeda;
+    }
+
+    public String getNomeMoeda() {
+        return nomeMoeda;
+    }
+
+    public void setNomeMoeda(String nomeMoeda) {
+        this.nomeMoeda = nomeMoeda;
+    }
+
+    public String getTipoMoeda() {
+        return tipoMoeda;
+    }
+
+    public void setTipoMoeda(String tipoMoeda) {
+        this.tipoMoeda = tipoMoeda;
+    }
+
+    public String getLocalMoeda() {
+        return localMoeda;
+    }
+
+    public void setLocalMoeda(String localMoeda) {
+        this.localMoeda = localMoeda;
+    }
 }
